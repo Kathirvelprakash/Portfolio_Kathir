5bfb52b931b7a24b99268f0b3ad19353987908fd
Kathirvelprakash <kathirvelprakash.m@gmail.com>
2025-03-19 17:38:17 +0530

    New files are Udated

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['MCA Student ','Python Developer'],
+    strings:['MCA Student ','PHP Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
